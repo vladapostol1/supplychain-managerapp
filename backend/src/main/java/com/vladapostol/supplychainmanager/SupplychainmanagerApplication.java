911557c3b8320324994c7ae0807d5c78d1c4b97c
vladapostol1 <vladapostol1029@gmail.com>
2023-11-15 14:29:41 +0200

    Init backend

---
+++ backend/src/main/java/com/vladapostol/supplychainmanager/SupplychainmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.vladapostol.supplychainmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplychainmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplychainmanagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vladapostol/supplychainmanager/SupplychainmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladapostol.supplychainmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplychainmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
